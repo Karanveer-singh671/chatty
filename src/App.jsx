4bcbb7206614ce0a1d21b27de287dcf7df7ee99c
Karanveer-singh671 <singhkaranveer671@gmail.com>
2017-11-29 17:17:40 +0000

    multiple messages by same user

--- src/App.jsx
+++ src/App.jsx
@@ -4,42 +4,46 @@ import MessageList from './MessageList.jsx';
 import Navbar from './nav.jsx'
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-  messages: [
-    {
-      id:'1',
-      username: "Bob", 
-      content: "Has anyone seen my marbles?",
-    },
-    {
-      id:'2',
-      username: "Anonymous",
-      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+    this.socket = new WebSocket("ws://localhost:3001");
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+          id:'3'
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
+          id: '2'
+        }
+      ]
     }
-  ]
-    };
+    this.newChatMessage = this.newChatMessage.bind(this);
   }
-  componentDidMount() {
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
+    componentDidMount() {
+      this.socket.onopen = (event) => {
+        console.log('Connected to server');
   }
+}
+  
+  newChatMessage(content) {
+    const newMessage = {type: "postMessage", username: content.username, content: content.content};
+    const messages = this.state.messages.concat(newMessage)
+    this.setState({messages: messages})
+  }
+
   render() {
+    console.log("Rendering <App/>");
     return (
       <div>
         <Navbar />
-        <MessageList messages={this.state.messages} />  
-        <Chatbar currentUser={this.state.currentUser}/>
+        <MessageList messages={ this.state.messages }/>
+        <Chatbar currentUser={ this.state.currentUser}
+          newChatMessage={this.newChatMessage} />
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,13 +1,52 @@
 import React, {Component} from 'react';
 
 class Chatbar extends Component {
-  render() {
-    return (
+    constructor(props) {
+      super(props);
+      this.state = {
+        username : props.currentUser.name,
+        content: ''
+      }
+      this.onEnterKey = this.onEnterKey.bind(this);
+      this.onNewContent = this.onNewContent.bind(this);
+     this.newChatMessage = this.props.newChatMessage.bind(this);
+    }
+  
+    onNewContent(event) {
+      this.setState({
+        content: event.target.value
+      });
+    }
+    onNewName(event) {
+        this.setState({username: event.target.value});
+    }
+
+    onEnterNewName(event) {
+        if(event.key === "Enter") {
+        this.props.currentUser(this.state);
+        this.onNewName(event);
+      }
+    }
+  
+    onEnterKey(event) {
+       if(event.key == 'Enter'){
+         this.props.newChatMessage(this.state);
+         this.setState({content:''})
+       }
+    }
+  
+    render() {
+      return (
         <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-      </footer>
-    );
+          <input className="chatbar-username" placeholder={this.state.username} 
+          onChange={this.onNewName} 
+          onKeyPress={this.onEnterNewName}/>
+          <input className="chatbar-message" placeholder="Type a message and hit ENTER" 
+            onChange={this.onNewContent} value={this.state.content} 
+            onKeyPress={this.onEnterKey} /> 
+        </footer>
+      );
+    }
   }
-}
-export default Chatbar;
+  
+  export default Chatbar;
