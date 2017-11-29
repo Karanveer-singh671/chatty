ffcf1d573930971ec4f766b1b39caa7a189f32fc
Karanveer-singh671 <singhkaranveer671@gmail.com>
2017-11-29 18:24:03 +0000

    fixed key id error

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,42 @@ import React, {Component} from 'react';
 import Chatbar from './ChatBar.jsx'
 import MessageList from './MessageList.jsx';
 import Navbar from './nav.jsx'
+import home from '../styles/home.scss';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.socket = new WebSocket("ws://localhost:3001");
+    this.socket = null;
     this.state = {
       currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
       messages: [
         {
           username: "Bob",
           content: "Has anyone seen my marbles?",
-          id:'3'
+          id: 1
         },
         {
           username: "Anonymous",
           content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
-          id: '2'
+          id: 2
         }
       ]
     }
     this.newChatMessage = this.newChatMessage.bind(this);
   }
-    componentDidMount() {
-      this.socket.onopen = (event) => {
-        console.log('Connected to server');
+
+
+  componentDidMount() {
+    this.socket = new WebSocket("ws://localhost:3001");
   }
-}
-  
+
+
   newChatMessage(content) {
-    const newMessage = {type: "postMessage", username: content.username, content: content.content};
+    console.log("we are able to receive the content ",content);
+    const id = this.state.messages[this.state.messages.length - 1].id + 1 
+    console.log('id',id);
+    const newMessage = {id: id, username: "thomas", content: content.content};
     const messages = this.state.messages.concat(newMessage)
     this.setState({messages: messages})
   }
@@ -47,5 +53,7 @@ class App extends Component {
       </div>
     );
   }
+
 }
+
 export default App;
